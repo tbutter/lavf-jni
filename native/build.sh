cfc6b31ec846e37dea70eec17f70b3619a6bccb1
tbutter <thomas@gmx.net>
2021-12-01 23:18:31 +0000

    buildscripts

--- native/build.sh
+++ native/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cp build/generated/sources/headers/java/main/com_blubb_lavf_LAVFNative.h native
+cp ../build/generated/sources/headers/java/main/com_blubb_lavf_LAVFNative.h .
 
 cd native
 docker build -t lavfbuild .
 CID=`docker create lavfbuild`
 docker cp $CID:/lavf/liblavf.so liblavf.so
+docker cp $CID:/work/include .
 mkdir -p ../src/main/resources/
 cp liblavf.so ../src/main/resources/
 echo copied
